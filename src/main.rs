4d02c2c9820771869e98bed8ae49bc7178256b28
NazgoooAtanasov <naskodragomirov@gmail.com>
2023-03-04 15:29:53 +0200

    start of the rewrite
    1. rewrote the cartridge detection;
    2. rewrote the zipping of files.

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,217 @@
-use std::{
-    collections::HashMap,
-    process::Command
-};
+use std::collections::HashMap;
+use std::fs;
+use std::path;
+use std::io::{Read, Write};
 
-use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent};
-use std::sync::mpsc::channel;
-use std::time::Duration;
+type Cartridges = Vec<String>;
 
-mod webdav;
-mod parser;
-mod directories;
+struct Uploader {
+    f_watch: bool,
+    f_upload: bool,
+    working_dir: String,
+    arguments: HashMap<String, String>,
+    cartridges: Cartridges
+}
 
-fn manage(filepath: &str) {
-    let path = std::path::Path::new(filepath);
-
-    let mut webdav_client = webdav::WebdavClient::new();
-    webdav_client.set_config(format!("{}/dw.json", filepath).as_str());
-
-    let mut cartridges_metadata: HashMap<String, String> = HashMap::new();
-
-    let forbidden_paths: Vec<String> = vec!["node_modules".to_string(), "target".to_string(), "build-suite".to_string(), "git".to_string()];
-
-    let _ = directories::walk_directories(
-        path,
-        &mut cartridges_metadata,
-        &forbidden_paths,
-        &webdav_client
-    );
-
-    let mut threads: Vec<std::thread::JoinHandle<()>> = Vec::new();
-
-    for (name, path) in cartridges_metadata {
-        let mut cartridge_parent_path_vec: Vec<&str> = path.split("/").collect();
-        cartridge_parent_path_vec.pop();
-
-        let cartridges_parent_path = cartridge_parent_path_vec.join("/");
-
-        println!("Zipping, uploading and unzipping: [{}]", name);
-
-        let _out = Command::new("sh")
-            .current_dir(&cartridges_parent_path)
-            .arg("-c")
-            .arg(format!("zip {}.zip {} -r", name, name))
-            .output();
-
-        webdav_client.send_cartridge(&cartridges_parent_path, &name);
-
-        let webdav_client_clone = webdav_client.clone();
-        println!("[WATCHER] Starting to watch for changes in [{}]", name);
-        let thread = std::thread::spawn(move || {
-            let (tx, rx) = channel();
-            let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
-            watcher.watch(path, RecursiveMode::Recursive).unwrap();
-
-            loop {
-                match rx.recv() {
-                    Ok(DebouncedEvent::Write(path)) | Ok(DebouncedEvent::Create(path)) => {
-                        let sanitized_webdav_path = directories::sanitize_webdav_path(path.to_str().unwrap());
-
-                        println!("[ADDITION/CHANGE] Uploading: [{}]", sanitized_webdav_path);
-                        if sanitized_webdav_path.contains(".") {
-                            webdav_client_clone.upload_file(
-                                path.to_str().unwrap(),
-                                &sanitized_webdav_path
-                            );
-                        } else {
-                            webdav_client_clone.create_directory(&sanitized_webdav_path);
-                        }
-                    },
-                    Ok(DebouncedEvent::Remove(path)) => {
-                        let sanitized_webdav_path = directories::sanitize_webdav_path(path.to_str().unwrap());
-                        println!("[DELETION] Deleting [{}]", sanitized_webdav_path);
-                        webdav_client_clone.delete(&sanitized_webdav_path);
-                    }
-                    Ok(DebouncedEvent::Rename(source, dest)) => {
-                        let sanitized_webdav_path_source = directories::sanitize_webdav_path(source.to_str().unwrap());
-                        let sanitized_webdav_path_dest = directories::sanitize_webdav_path(dest.to_str().unwrap());
-
-                        println!("[MOVE] Moving [{}] -> [{}]", sanitized_webdav_path_source, sanitized_webdav_path_dest);
-                        webdav_client_clone.delete(&sanitized_webdav_path_source);
-                        webdav_client_clone.upload_file(
-                            dest.to_str().unwrap(),
-                            &sanitized_webdav_path_dest
-                        );
-                    },
-                    Ok(_event) => {},
-                    Err(e) => println!("err {:?}", e)
-                }
-            };
-        });
-
-        threads.push(thread);
+impl Uploader {
+    fn new() -> Self {
+        return Self {
+            f_upload: true,
+            f_watch: true,
+            working_dir: String::new(),
+            arguments: HashMap::new(),
+            cartridges: Cartridges::default()
+        }
     }
 
-    for thread in threads {
-        thread.join().expect("Error in joining thread");
+    fn parse_args(mut self, command_args: std::env::Args) -> Self {
+        let mut expect_value = false;
+        let mut expect_value_buffer = String::new();
+        for (n, arg) in command_args.skip(1).enumerate() {
+            if n == 0 {
+                self.working_dir = arg;
+            } else {
+                match arg.as_str() {
+                    "-w" | "-u" => {
+                        self.arguments.insert(arg, String::new());
+                    },
+
+                    "--config" => {
+                        expect_value = true;
+                        expect_value_buffer = arg;
+                    }
+
+                    anything => {
+                        if expect_value && !expect_value_buffer.is_empty() {
+                            self.arguments.insert(expect_value_buffer.clone(), String::from(anything));
+                            expect_value = false;
+                            expect_value_buffer = String::new();
+                        }
+                    }
+                }
+            }
+        }
+
+        return self;
+    }
+
+    fn set_flags(mut self) -> Self {
+        for (flag, _value) in self.arguments.clone() {
+            match flag.as_str() {
+                "-w" => {
+                    self.f_watch = true;
+                }
+
+                "-u" => {
+                    self.f_upload = true;
+                }
+
+                _ => panic!("Flag {flag} not supported")
+            }
+        }
+
+        return self;
+    }
+
+    fn get_cartridges(&self, working_dir: String) -> Cartridges {
+        let mut cartridges = Cartridges::default();
+
+        let is_project = fs::read_dir(path::Path::new(&working_dir))
+            .expect(format!("Problem with opening passed dir {}", working_dir).as_str())
+            .map(|x| {
+                return x.unwrap();
+            })
+            .any(|x| {
+                return x.path().to_str().unwrap().contains(".project");
+            });
+
+        if is_project {
+            cartridges.push(working_dir);
+        } else {
+            fs::read_dir(path::Path::new(&working_dir)) // refetching the whole dir for now. research how to reuse an iterator for above an here.
+                .expect(format!("Problem with opening passed dir {}", working_dir).as_str())
+                .map(|x| {
+                    return x.unwrap();
+                })
+                .filter(|x| {
+                    return x.path().is_dir();
+                })
+                .for_each(|x| {
+                    cartridges.append(&mut self.get_cartridges(x.path().to_str().unwrap().to_string()));
+                });
+        }
+
+        return cartridges;
+    }
+
+    fn set_cartridges(mut self) -> Self {
+        self.cartridges = self.get_cartridges(self.working_dir.clone());
+        return self;
+    }
+}
+
+
+type ZipFiles = HashMap<String, String>;
+struct ZipHanlder {
+    options: zip::write::FileOptions,
+    zip_files: ZipFiles,
+    output_dir: String
+}
+
+impl ZipHanlder {
+    fn new() -> Self {
+        return Self {
+            zip_files: ZipFiles::default(),
+            output_dir: String::from("./outdir"),
+            options: zip::write::FileOptions::default()
+                .compression_method(zip::CompressionMethod::Stored)
+                .unix_permissions(0o755)
+        };
+    }
+
+    fn reset_outdir(self) -> Self {
+        let old_zips = fs::read_dir(path::Path::new(self.output_dir.as_str())).unwrap();
+        for zip in old_zips {
+            fs::remove_file(zip.unwrap().path()).unwrap();
+        }
+        return self;
+    }
+
+    fn zip_entries(
+        &self,
+        zip: &mut zip::ZipWriter<fs::File>,
+        options: &zip::write::FileOptions,
+        current_cartridge_name: &String,
+        current_cartridge_path: &String,
+        entries: fs::ReadDir
+    ) -> zip::result::ZipResult<()> {
+        let entries = entries.map(|x| { return x.unwrap(); });
+        let mut buffer = Vec::new();
+
+        for entry in entries {
+            let path = entry.path();
+            let mut name = path
+                .to_str()
+                .unwrap()
+                .replace(current_cartridge_path, "");
+            name.insert_str(0, current_cartridge_name);
+
+            println!("{name}, {}", path.to_str().unwrap());
+
+            if path.is_file() {
+                zip.start_file(name, *options)?;
+                let mut f = fs::File::open(path)?;
+
+                f.read_to_end(&mut buffer)?;
+                zip.write_all(&buffer)?;
+                buffer.clear();
+            } else {
+                zip.add_directory(name, *options)?;
+
+                let new_entries = fs::read_dir(path)?;
+                self.zip_entries(zip, options, current_cartridge_name, current_cartridge_path, new_entries)?;
+            }
+        }
+
+        return Ok(());
+    }
+
+    fn zip(mut self, cartridges: &Cartridges) -> zip::result::ZipResult<Self> {
+        for cartridge in cartridges.iter() {
+            let cartridge_name = cartridge
+                .split("/")
+                .collect::<Vec<&str>>()
+                .last()
+                .unwrap()
+                .clone(); // ???
+
+            // check for outdirs existance before writing to it
+            let zip_file_paht_str = String::from(format!("{}/{}.zip", self.output_dir, cartridge_name));
+            let zip_file_path = path::Path::new(zip_file_paht_str.as_str());
+            let zip_file = fs::File::create(zip_file_path).unwrap();
+            let mut zip = zip::ZipWriter::new(zip_file);
+
+            let cartridge_dir_path = path::Path::new(cartridge);
+            let dir_entries = fs::read_dir(cartridge_dir_path);
+
+            if let Ok(entries) = dir_entries {
+                self.zip_entries(&mut zip, &self.options, &String::from(cartridge_name), &cartridge, entries)?;
+                self.zip_files.insert(String::from(cartridge_name), cartridge.clone());
+                zip.finish().unwrap();
+            }
+        }
+
+        return Ok(self);
     }
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let filepath = &args[1];
+    let uploader: Uploader = Uploader::new()
+        .parse_args(std::env::args())
+        .set_flags()
+        .set_cartridges();
 
-    manage(filepath);
+    let _zip_handler: ZipHanlder = ZipHanlder::new()
+        .reset_outdir()
+        .zip(&uploader.cartridges).unwrap();
 }
--- src/parser.rs
+++
@@ -1,8 +0,0 @@
-use crate::webdav;
-
-pub fn parse_config(config_file: &str) -> webdav::Config {
-    let file = std::fs::read_to_string(config_file).unwrap();
-    let config: webdav::Config = serde_json::from_str(&file).unwrap();
-
-    config
-} 
